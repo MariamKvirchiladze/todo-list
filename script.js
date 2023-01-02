3f329f5c87f10d15cecf869ae80e7ee903bacd4d
MariamKvirchiladze <114876435+MariamKvirchiladze@users.noreply.github.com>
2023-01-03 01:19:31 +0400

    Add files via upload

---
+++ script.js
@@ -0,0 +1,134 @@
+`use strict`;
+window.addEventListener(`load`, () => {
+  todos = JSON.parse(localStorage.getItem(`todos`)) || [];
+  const nameInput = document.querySelector(`#name`);
+  const newTodoForm = document.querySelector(`#new-todo-form`);
+  const username = localStorage.getItem(`username`) || "";
+
+  nameInput.value = username;
+
+  nameInput.addEventListener(`change`, (e) => {
+    localStorage.setItem(`username`, e.target.value);
+  });
+
+  newTodoForm.addEventListener(`submit`, (e) => {
+    e.preventDefault();
+
+    const todo = {
+      content: e.target.elements.content.value,
+      category: e.target.elements.category.value,
+      done: false,
+      createdAt: new Date().getTime(),
+    };
+    todos.push(todo);
+
+    localStorage.setItem(`todos`, JSON.stringify(todos));
+
+    e.target.reset();
+
+    displayTodos();
+  });
+
+  displayTodos();
+});
+
+function displayTodos() {
+  const todoList = document.querySelector(`#todo-list`);
+
+  todoList.innerHTML = "";
+
+  todos.forEach((todo) => {
+    const todoItem = document.createElement(`div`);
+    todoItem.classList.add(`todo-item`);
+
+    const label = document.createElement(`label`);
+    const input = document.createElement(`input`);
+    const span = document.createElement(`span`);
+    const content = document.createElement(`div`);
+    const actions = document.createElement(`div`);
+    const edit = document.createElement(`button`);
+    const deleteButton = document.createElement(`button`);
+
+    input.type = `checkbox`;
+    input.checked = todo.done;
+    span.classList.add(`color`);
+
+    if (todo.category == `low`) {
+      span.classList.add(`low`);
+    } else if (todo.category == `medium`) {
+      span.classList.add(`medium`);
+    } else {
+      span.classList.add(`high`);
+    }
+
+    content.classList.add(`todo-content`);
+    actions.classList.add(`actions`);
+    edit.classList.add(`edit`);
+    deleteButton.classList.add(`delete`);
+
+    content.innerHTML = `<input type="text" value="${todo.content}" readonly>`;
+    edit.innerHTML = `EDIT`;
+    deleteButton.innerHTML = `DELETE`;
+
+    label.appendChild(input);
+    label.appendChild(span);
+    actions.appendChild(edit);
+    actions.appendChild(deleteButton);
+    todoItem.appendChild(label);
+    todoItem.appendChild(content);
+    todoItem.appendChild(actions);
+    todoList.appendChild(todoItem);
+
+    if (todo.done) {
+      todoItem.classList.add(`done`);
+    }
+
+    input.addEventListener(`click`, (e) => {
+      todo.done = e.target.checked;
+      localStorage.setItem(`todos`, JSON.stringify(todos));
+
+      if (todo.done) {
+        todoItem.classList.add(`done`);
+      } else {
+        todoItem.classList.remove(`done`);
+      }
+      displayTodos();
+    });
+
+    edit.addEventListener(`click`, (e) => {
+      const input = content.querySelector(`input`);
+      input.removeAttribute(`readonly`);
+      input.focus();
+      input.addEventListener(`blur`, (e) => {
+        input.setAttribute(`readonly`, true);
+        todo.content = e.target.value;
+        localStorage.setItem(`todos`, JSON.stringify(todos));
+        displayTodos();
+      });
+    });
+    deleteButton.addEventListener(`click`, (e) => {
+      todos = todos.filter((t) => t != todo);
+      localStorage.setItem(`todos`, JSON.stringify(todos));
+      displayTodos();
+    });
+    const darkmode = document.querySelector(`.dark`);
+    const lightmode = document.querySelector(`.light`);
+    const body = document.querySelector(`body`);
+    darkmode.addEventListener(`click`, (e) => {
+      body.classList.add(`dark-mode`);
+      darkmode.style.display = `none`;
+      lightmode.style.display = `block`;
+
+      localStorage.setItem(`todos`, JSON.stringify(todos));
+      displayTodos();
+    });
+    lightmode.addEventListener(`click`, (e) => {
+      body.classList.remove(`dark-mode`);
+      darkmode.style.display = `block`;
+      lightmode.style.display = `none`;
+
+      localStorage.setItem(`todos`, JSON.stringify(todos));
+      displayTodos();
+    });
+  });
+}
